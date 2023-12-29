60bbc640a4b8dd2a11333732fe37950350963caf
wallison f. oliveira <wallisonfreitas00@gmail.com>
2023-12-29 16:50:26 -0300

    desafio-aula-01

--- A-partir-do-zero_Iniciante-em-programacao/logica-de-programacao/Material de Apoio/logica-js-projeto_inicial/app.js
+++ A-partir-do-zero_Iniciante-em-programacao/logica-de-programacao/Material de Apoio/logica-js-projeto_inicial/app.js
@@ -1,4 +1,4 @@
-alert('Boas vindas ao jogo do número secreto'); //Alerta apresentado
+/*alert('Boas vindas ao jogo do número secreto'); //Alerta apresentado
 let numeroSecreto = 3; //Criando variáreis
 
 let chute = prompt('Escolha um número entre 1 e 10:');
@@ -7,4 +7,26 @@ if (numeroSecreto == chute){
     console.log('Isso ai, você descobriu o número secreto! ('+ numeroSecreto +')');
 } else {
     console.log('Tente novamente.');
+}*/
+
+//Desafio 
+
+alert('Boas vindas ao nosso site!');
+
+let nome = 'Lua';
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+
+alert('Erro! Preencha todos os campos.');
+
+let mensagemDeErro = 'Erro! Preencha todos os campos.';
+
+alert(mensagemDeErro);
+
+nome = prompt('Qual o seu nome:');
+idade = prompt('Sua idade: ');
+
+if (idade >= 18){
+    alert('Pode tirar a habilitação');
 }
