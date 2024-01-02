237a6ee3eba39919d1b28d548a430833b53129ae
wallison f. oliveira <wallisonfreitas00@gmail.com>
2024-01-02 14:36:35 -0300

    aula de while

--- A-partir-do-zero_Iniciante-em-programacao/logica-de-programacao/Material de Apoio/logica-js-projeto_inicial/app.js
+++ A-partir-do-zero_Iniciante-em-programacao/logica-de-programacao/Material de Apoio/logica-js-projeto_inicial/app.js
@@ -1,16 +1,27 @@
 
 alert(`Boas vindas ao jogo do número secreto`); //Alerta apresentado
 let numeroSecreto = 3; //Criando variáreis
+let tentativas = 0;
 
 console.log(`Número Secreto: (${numeroSecreto})`);
 
-let chute = prompt(`Escolha um número entre 1 e 10:`);
+let chute;
 console.log(`Número Preenchido: (${chute})`);
 
-if (numeroSecreto == chute){
-    //Para apresentar uma string não é preciso ter as pas simples, o usuo da crase `` substitui, e para declarar uma variável na string é utilizado ${variável}
-    console.log(`Isso ai, você descobriu o número secreto! (${numeroSecreto})`); 
-    alert(`Isso ai, você descobriu o número secreto! (${numeroSecreto})`);
-} else {
-    console.log(`Você errou, tente novamente.`);
+// enquanto == while
+while(chute != numeroSecreto){
+    chute = prompt(`Escolha um número entre 1 e 10:`);
+    tentativas ++;
+    
+    if(numeroSecreto == chute){
+        alert(`Isso ai, você descobriu o número secreto (${numeroSecreto}) na ${tentativas}º tentativa.`);
+    }else{
+        if(numeroSecreto > chute){
+            alert(`${tentativas}º Tentativa - O número secreto é MAIOR que (${chute})! Tente novamente.`); 
+        }else{
+            if(numeroSecreto < chute){
+                alert(`${tentativas}º Tentativa - O número secreto é MENOR que (${chute})! Tente novamente.`);
+            }
+        }
+    }
 }
