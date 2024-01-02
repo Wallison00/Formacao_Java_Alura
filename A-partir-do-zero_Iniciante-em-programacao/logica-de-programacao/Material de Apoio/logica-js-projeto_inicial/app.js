ef55a7fbe9810b81bd5e48b69fc1b55679b0e51f
wallison f. oliveira <wallisonfreitas00@gmail.com>
2024-01-02 11:33:40 -0300

    Criação da pasta desafio-02 + correções

--- A-partir-do-zero_Iniciante-em-programacao/logica-de-programacao/Material de Apoio/logica-js-projeto_inicial/app.js
+++ A-partir-do-zero_Iniciante-em-programacao/logica-de-programacao/Material de Apoio/logica-js-projeto_inicial/app.js
@@ -2,9 +2,10 @@
 alert(`Boas vindas ao jogo do número secreto`); //Alerta apresentado
 let numeroSecreto = 3; //Criando variáreis
 
-console.log(`Número preenchido: (${numeroSecreto})`);
+console.log(`Número Secreto: (${numeroSecreto})`);
 
 let chute = prompt(`Escolha um número entre 1 e 10:`);
+console.log(`Número Preenchido: (${chute})`);
 
 if (numeroSecreto == chute){
     //Para apresentar uma string não é preciso ter as pas simples, o usuo da crase `` substitui, e para declarar uma variável na string é utilizado ${variável}
---
+++ A-partir-do-zero_Iniciante-em-programacao/logica-de-programacao/Material de Apoio/logica-js-projeto_inicial/desafio-aula-02/app.js
@@ -0,0 +1,60 @@
+//Desafio
+
+//1 - Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
+
+/*let dia = prompt(`Qual o dia da semana:`);
+console.log(`Dia digitado: (${dia})`);
+
+if(dia == `Sábado` || dia == `Domingo`){
+    alert(`Bom fim de semana`);
+}else{
+    alert(`O dia da semana não é o esperado.`);
+}*/
+
+//2 - Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
+/*
+let numeroDigitado = prompt(`Digite um número: `);
+console.log(`O número digitado foi: (${numeroDigitado})`);
+
+if(numeroDigitado > 0){
+    alert(`O número (${numeroDigitado}) é positivo!`);
+}else{
+    if(numeroDigitado < 0){
+        alert(`O número (${numeroDigitado}) é negativo`);
+    }else{
+        if(numeroDigitado = 0){
+            alert(`O número (${numeroDigitado}) é nulo`);
+        }else{
+            alert(`Erro desconhecido!`);
+        }
+    }
+}*/
+
+//3 - Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!". Caso contrário, mostre "Tente novamente para ganhar.".
+/*
+let pontuacao = 10;
+console.log(`pontuacao informada: (${pontuacao})`);
+
+if(pontuacao >= 100){
+    alert(`Parabéns, você venceu!`);
+}else{
+    if(pontuacao < 100){
+        alert(`Tente novamente para ganhar!`);
+    }else{
+        alert(`Erro desconhecido!`);
+    }
+}
+*/
+
+//4 - Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.
+/*
+let saldo = prompt(`Informe o valor do seu saldo:`);
+console.log(`O seu saldo é de: (${saldo}).`);
+alert(`O seu saldo é de: (${saldo}).`);
+*/
+
+//5 - Peça ao usuário para inserir seu nome usando prompt. Em seguida, mostre um alerta de boas-vindas usando esse nome.
+/*
+let nome = prompt(`Informe seu nome:`);
+alert(`Olá ${nome}, seja bem-vindo!`);
+*/
