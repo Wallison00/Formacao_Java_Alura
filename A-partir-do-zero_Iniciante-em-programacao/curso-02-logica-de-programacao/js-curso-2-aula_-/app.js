92f43609e67dd2b14c41db065582555e4020942b
Wallison Freitas de Oliveira <wallisonfreitas00@gmail.com>
2024-01-08 20:56:06 -0300

    Manipular textos HTML em JavaScript

---
+++ A-partir-do-zero_Iniciante-em-programacao/curso-02-logica-de-programacao/js-curso-2-aula_-/app.js
@@ -0,0 +1,6 @@
+// apontar para o html
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do Número Secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
