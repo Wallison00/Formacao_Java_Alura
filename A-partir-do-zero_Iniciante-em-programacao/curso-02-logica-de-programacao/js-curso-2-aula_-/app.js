7c46f2555bcdf77afe609194004746bfd473c80c
wallison f. oliveira <wallisonfreitas00@gmail.com>
2024-01-09 14:13:54 -0300

    Desafio Aula-02

--- A-partir-do-zero_Iniciante-em-programacao/curso-02-logica-de-programacao/js-curso-2-aula_-/app.js
+++ A-partir-do-zero_Iniciante-em-programacao/curso-02-logica-de-programacao/js-curso-2-aula_-/app.js
@@ -1,52 +1,65 @@
-// apontar para o html
-//let titulo = document.querySelector('h1');
-//titulo.innerHTML = 'Jogo do Número Secreto';
+//Desafio Aula-02
 
-//let paragrafo = document.querySelector('p');
-//paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+//Criar uma função que exibe "Olá, mundo!" no console.
+/*
+function olaMundo(){
+    console.log('Olá mundo');
+}
 
-//Criar uma função 
-//Verificar se a função verificarChute foi clicada no HTML
+olaMundo();
+*/
 
-function verificarChute(){ //Função sem parâmetro e sem retorno
-    console.log('O botão foi clicado!');
+//Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
+/*
+function ola(nome){
+    console.log(`Olá, ${nome}`);
+}
+
+ola('Wallison');
+*/
+
+//Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
+/*
+function dobro(numero){
+    console.log(numero * 2);
+}
+
+dobro(6);
+*/
+
+//Criar uma função que recebe três números como parâmetros e retorna a média deles.
+/*
+function media(n1, n2, n3){
+    console.log((n1 + n2 + n3)/3);
+}
+
+media(5,10,5);
+*/
+
+//Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
+/*
+function maiorNumero(numero1, numero2){
+    if(numero1 == numero2){
+        return 'Erro';
+    }else{
+        if(numero1 > numero2){
+            return numero1;
+        }else{
+            return numero2;
+        }
+    }
 };
 
+let resultado = maiorNumero(5,2);
 
+console.log(resultado);
+*/
 
-//Parâmetros com funções
-function exibirTextoNaTela(tag, texto){ //Função com parâmetro e sem retorno
-    let campo = document.querySelector(tag);
-    campo.innerHTML = texto;
-}
+//Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo
+/*
+function multiplicacao(numero){
+    return numero * numero;
+};
 
-exibirTextoNaTela('h1', 'Jogo do Número Secreto!');//Execução da função através dos parâmetros
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10.');
-
-
-
-//Função sem parametro com retorno.
-let numeroSecreto = gerarNumeroAleatorio();
-
-function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10 + 1);
-}
-
-
-//Exemplo acima
-function chamadaFuncao(){
-    //console.log(gerarNumeroAleatorio()); //Chama direto a função
-    console.log(numeroSecreto); //Repete o valor capturado pela função
-}
-
-
-//Capturando informação digitada
-function informacaoDigitada(){
-    let digitada = document.querySelector('input').value;
-    console.log(`O valor digitado foi (${digitada})`);
-}
-
-
-
-
-let captura = document.getElementsByClassName('input');
+console.log(multiplicacao(1));
+*/
