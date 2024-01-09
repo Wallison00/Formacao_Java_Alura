f3122fdb5f5836df63461c4e7a32835296ad8167
Wallison Freitas de Oliveira <wallisonfreitas00@gmail.com>
2024-01-08 21:11:04 -0300

    Criando funcoes no javascript

--- A-partir-do-zero_Iniciante-em-programacao/curso-02-logica-de-programacao/js-curso-2-aula_-/app.js
+++ A-partir-do-zero_Iniciante-em-programacao/curso-02-logica-de-programacao/js-curso-2-aula_-/app.js
@@ -4,3 +4,9 @@ titulo.innerHTML = 'Jogo do Número Secreto';
 
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+//Criar uma função 
+//Verificar se a função verificarChute foi clicada no HTML
+function verificarChute(){
+    console.log('O botão foi clicado!');
+};
