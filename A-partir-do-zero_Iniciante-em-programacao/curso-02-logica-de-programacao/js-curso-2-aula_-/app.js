6208cc12891d3ba3c9210d2501f755fd62658bf7
wallison f. oliveira <wallisonfreitas00@gmail.com>
2024-01-09 13:05:01 -0300

    funções com parâmetros

--- A-partir-do-zero_Iniciante-em-programacao/curso-02-logica-de-programacao/js-curso-2-aula_-/app.js
+++ A-partir-do-zero_Iniciante-em-programacao/curso-02-logica-de-programacao/js-curso-2-aula_-/app.js
@@ -1,12 +1,21 @@
 // apontar para o html
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do Número Secreto';
+//let titulo = document.querySelector('h1');
+//titulo.innerHTML = 'Jogo do Número Secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+//let paragrafo = document.querySelector('p');
+//paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
 //Criar uma função 
 //Verificar se a função verificarChute foi clicada no HTML
 function verificarChute(){
     console.log('O botão foi clicado!');
 };
+
+//Parâmetros com funções
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do Número Secreto!');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10.');
