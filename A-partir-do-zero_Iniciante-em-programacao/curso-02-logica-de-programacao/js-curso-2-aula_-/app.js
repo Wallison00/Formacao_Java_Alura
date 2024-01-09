f140ba9665e9b0551795e24666868e703a52b46b
wallison f. oliveira <wallisonfreitas00@gmail.com>
2024-01-09 13:25:18 -0300

    função com retorno

--- A-partir-do-zero_Iniciante-em-programacao/curso-02-logica-de-programacao/js-curso-2-aula_-/app.js
+++ A-partir-do-zero_Iniciante-em-programacao/curso-02-logica-de-programacao/js-curso-2-aula_-/app.js
@@ -7,15 +7,27 @@
 
 //Criar uma função 
 //Verificar se a função verificarChute foi clicada no HTML
-function verificarChute(){
+function verificarChute(){ //Função sem parâmetro e sem retorno
     console.log('O botão foi clicado!');
 };
 
+
+
 //Parâmetros com funções
-function exibirTextoNaTela(tag, texto){
+function exibirTextoNaTela(tag, texto){ //Função com parâmetro e sem retorno
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1', 'Jogo do Número Secreto!');
+exibirTextoNaTela('h1', 'Jogo do Número Secreto!');//Execução da função através dos parâmetros
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10.');
+
+
+
+//Função sem parametro com retorno.
+let numeroSecreto = gerarNumeroAleatorio();
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
+
