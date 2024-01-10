0e11681618bf63c6efef05133fbdf89bd4565925
wallison f. oliveira <wallisonfreitas00@gmail.com>
2024-01-10 19:05:34 -0300

    desafio em andamento

--- A-partir-do-zero_Iniciante-em-programacao/curso-02-logica-de-programacao/js-curso-2-desafio_1/app.js
+++ A-partir-do-zero_Iniciante-em-programacao/curso-02-logica-de-programacao/js-curso-2-desafio_1/app.js
@@ -1,5 +1,5 @@
 //Desafio
-
+/*
 //Altere o conteúdo da tag h1 com document.querySelector e atribua o seguinte texto: Hora do Desafio.
 let titulo = document.querySelector('h1');
 titulo.innerHTML = 'Hora do Desafio!';
@@ -30,3 +30,57 @@ function soma(){
 
     alert(`O resultado da soma é ${resultado}`);
 }
+*/
+
+//Desafio Aula 3
+
+// 1 - Crie uma função que calcule o índice de massa corporal (IMC) de uma pessoa, a partir de sua altura, em metros, e peso, 
+//em quilogramas, que serão recebidos como parâmetro.
+/*
+function calculadoraIMC(altura, peso){
+    return imc = peso/(altura * altura);
+}
+
+resultado = calculadoraIMC('1.70', '112');
+console.log(resultado);
+*/
+
+// 2 - Crie uma função que calcule o valor do fatorial de um número passado como parâmetro.
+
+fatorial(4);
+
+function fatorial(numero){ //4
+    
+    multiplicador = numero - 1; //3
+    
+    /*
+    calculo = numero * multiplicador;
+    console.log(`Calculo realizado: ${calculo}`);
+    */
+
+
+   while(multiplicador != 0){
+    //console.log(`Laço ${multiplicador} do while`);
+    console.log(calculadora(numero,multiplicador));
+    numero = calculadora(numero, multiplicador);
+    multiplicador--;
+    console.log(`O número ${numero}`);
+   }
+
+   console.log(calculadora(numero,multiplicador));
+};
+
+function calculadora(numero, multiplicador){
+    resultado = numero * multiplicador;
+    return resultado;
+}
+
+
+// 3 - Crie uma função que converte um valor em dólar, passado como parâmetro, e retorna o valor equivalente em reais. 
+//Para isso, considere a cotação do dólar igual a R$4,80.
+
+// 4 - Crie uma função que mostre na tela a área e o perímetro de uma sala retangular, utilizando altura e largura que serão dadas como parâmetro.
+
+// 5 - Crie uma função que mostre na tela a área e o perímetro de uma sala circular, utilizando seu raio que será fornecido como parâmetro. Considere Pi = 3,14.
+ 
+// 6 - Crie uma função que mostre na tela a tabuada de um número dado como parâmetro.
