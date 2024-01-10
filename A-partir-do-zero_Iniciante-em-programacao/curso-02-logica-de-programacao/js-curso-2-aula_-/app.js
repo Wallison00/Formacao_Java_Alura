f737927beb0a947d4b03cb1a33ee52451dfc8108
Wallison Freitas de Oliveira <wallisonfreitas00@gmail.com>
2024-01-09 22:22:10 -0300

    Remover atributo setando um elemento no JS

--- A-partir-do-zero_Iniciante-em-programacao/curso-02-logica-de-programacao/js-curso-2-aula_-/app.js
+++ A-partir-do-zero_Iniciante-em-programacao/curso-02-logica-de-programacao/js-curso-2-aula_-/app.js
@@ -16,12 +16,19 @@ let numeroSecreto = parseInt(Math.random() * 10 + 1);
 console.log(numeroSecreto);
 
 function verificarChute(){
-    let chute = document.querySelector('input').value;
-    tentativas++;
-    let mensagem = tentativas == 1 ? `Você encontrou o número correto com ${tentativas} tentativa!` : `Você encontrou o número correto com ${tentativas} tentativas!`;
     
+    let chute = document.querySelector('input').value;//Atribui um valor ao campo de preenchimento
+    tentativas++;
+
+    //apresenta a mensagem de quantidade das tentativas.
+    let mensagem = tentativas == 1 ? `Você encontrou o número correto com ${tentativas} tentativa!` : `Você encontrou o número correto com ${tentativas} tentativas!`;
+
     if(chute == numeroSecreto){
         exibirTextoNaTela('p', mensagem);
+        
+        //Buscar o elemento que tem id = reiniciar e remover o atributo desasabilitado do elemento
+        document.getElementById('reiniciar').removeAttribute('disabled');
+
     }else{
         if(chute < numeroSecreto){
             exibirTextoNaTela('p', `O numero secreto é MAIOR que ${chute}.`);
@@ -29,4 +36,12 @@ function verificarChute(){
             exibirTextoNaTela('p', `O numero secreto é MENOR que ${chute}.`);
         }
     }
+    //limpar campo do input
+    limparCampo();
 }
+
+//Limpar campo atribuindo um valor vazio ao campo
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
+};
