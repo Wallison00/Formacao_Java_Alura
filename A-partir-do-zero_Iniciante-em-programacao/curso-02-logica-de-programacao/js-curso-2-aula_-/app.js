cb55c27a8873c458b66fc9f88e5845dc562fdc7d
Wallison Freitas de Oliveira <wallisonfreitas00@gmail.com>
2024-01-09 21:39:48 -0300

    Verificando o chute

--- A-partir-do-zero_Iniciante-em-programacao/curso-02-logica-de-programacao/js-curso-2-aula_-/app.js
+++ A-partir-do-zero_Iniciante-em-programacao/curso-02-logica-de-programacao/js-curso-2-aula_-/app.js
@@ -1,65 +1,33 @@
-//Desafio Aula-02
 
-//Criar uma função que exibe "Olá, mundo!" no console.
-/*
-function olaMundo(){
-    console.log('Olá mundo');
+
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
 }
 
-olaMundo();
-*/
+exibirTextoNaTela('h1', 'Jogo do número secreto!');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10:')
 
-//Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
-/*
-function ola(nome){
-    console.log(`Olá, ${nome}`);
-}
 
-ola('Wallison');
-*/
 
-//Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
-/*
-function dobro(numero){
-    console.log(numero * 2);
-}
 
-dobro(6);
-*/
 
-//Criar uma função que recebe três números como parâmetros e retorna a média deles.
-/*
-function media(n1, n2, n3){
-    console.log((n1 + n2 + n3)/3);
-}
 
-media(5,10,5);
-*/
+//Verificando o chute
 
-//Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
-/*
-function maiorNumero(numero1, numero2){
-    if(numero1 == numero2){
-        return 'Erro';
+let numeroSecreto = parseInt(Math.random() * 10 + 1);
+console.log(numeroSecreto);
+
+function verificarChute(){
+    let chute = document.querySelector('input').value;
+
+    if(chute == numeroSecreto){
+        exibirTextoNaTela('p', 'Você encontrou o número correto!');
     }else{
-        if(numero1 > numero2){
-            return numero1;
+        if(chute < numeroSecreto){
+            exibirTextoNaTela('p', `O numero secreto é MAIOR que ${chute}.`);
         }else{
-            return numero2;
+            exibirTextoNaTela('p', `O numero secreto é MENOR que ${chute}.`);
         }
     }
-};
-
-let resultado = maiorNumero(5,2);
-
-console.log(resultado);
-*/
-
-//Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo
-/*
-function multiplicacao(numero){
-    return numero * numero;
-};
-
-console.log(multiplicacao(1));
-*/
+}
